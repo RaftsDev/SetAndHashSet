b229c5c97383e717cca85c3c3b9aee964ae690e1
Ralph F <RaftsDev@gmail.com>
2020-10-16 22:00:32 -0400

    Chore: Format output.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -103,6 +103,8 @@ public class Main {
         HeavenlyBody pluto = new HeavenlyBody("Pluto", 842);
         planets.add(pluto);
 
+        System.out.println("Planets: ");
+
         for( HeavenlyBody planet : planets){
             System.out.println(planet.getName() + ":" + planet.getOrbitalPeriod());
         }
