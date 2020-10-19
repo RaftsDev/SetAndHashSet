3471688dcdbd64740efc3c2a9617be4b14bce347
Ralph F <RaftsDev@gmail.com>
2020-10-18 20:35:00 -0400

    Fix: name property of instance.

--- src/com/company/DogMain.java
+++ src/com/company/DogMain.java
@@ -3,7 +3,7 @@ package com.company;
 public class DogMain {
     public static void main(String[] args) {
         Labrador rover = new Labrador("rover");
-        Dog rover2 = new Dog("rover2");
+        Dog rover2 = new Dog("rover");
 
         System.out.println(rover2.equals(rover));
         System.out.println(rover.equals(rover2));
