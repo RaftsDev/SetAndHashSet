c49023ab18e76a1545870a9ef235667724ad40ef
Ralph F <RaftsDev@gmail.com>
2020-10-18 20:10:15 -0400

    Feat: Create Class Dog.

---
+++ src/com/company/Dog.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Dog {
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj){
+            return true;
+        }
+        if(obj instanceof Dog){
+            String objName = ((Dog)obj).getName();
+            return this.getName().equals(obj);
+        }
+        return false;
+    }
+}
